d9358a5be6d0c7cc77ac9be994424dc6f9db8681
Bradley Bell <btb@icculus.org>
2015-03-25 17:18:59 -0700

    Not needed in NDEBUG. Fixes: similar/main/ai.cpp:4311:20: error: unused function 'dump_ai_objects_all'

--- similar/main/ai.cpp
+++ similar/main/ai.cpp
@@ -4242,6 +4242,7 @@ static void set_player_awareness_all(void)
 		}
 }
 
+#ifndef NDEBUG
 #if PARALLAX
 int Ai_dump_enable = 0;
 
@@ -4311,6 +4312,7 @@ void force_dump_ai_objects_all(const char *msg)
 static inline void dump_ai_objects_all()
 {
 }
+#endif
 #endif
 
 // ----------------------------------------------------------------------------------
